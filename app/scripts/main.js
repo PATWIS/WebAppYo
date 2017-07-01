5f43472d537a01ac5a0c4d864d37210ce14b08dc
Patryk Wiśniewski <Patryk Winiewski>
2017-07-01 20:22:14 +0200

    created login form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,17 @@
-let test = 'test';
+
+$('#submit').click(function(){
+
+   let login= $('#login').val(),
+       password = $('#password').val();
+
+  console.log(login + password)
+
+   if (login ==='admin' && password ==='123') {
+       alert('success');
+   } else{
+       alert('error');
+   }
+
+});
 
 
-console.log(test);
